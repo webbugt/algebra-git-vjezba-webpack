b1954dd15638d34c9da82886c61a337e986923a2
Ivan Doncevic Viskovic <idoncev@gmail.com>
2024-03-19 20:11:26 +0100

    add lodash

--- src/create-item.js
+++ src/create-item.js
@@ -1,10 +1,19 @@
+import truncate from 'lodash/truncate';
+import join from 'lodash/join';
+
 export function createItem (resultsDiv, title, imageUrl) {
   const wrapperDiv = document.createElement('div');
   resultsDiv.append(wrapperDiv);
 
   const titleElement = document.createElement('h2');
-  titleElement.innerHTML = title;
+  const combinedTitle = join(['Title:', title], ' ');
+  const truncatedTitle = truncate(combinedTitle, {
+    length: 40
+  });
+  titleElement.innerHTML = truncatedTitle;
   wrapperDiv.append(titleElement);
+
+  wrapperDiv.title = title;
 
   const imageElement = document.createElement('img');
   imageElement.src = imageUrl;
